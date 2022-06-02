b3b85c4a7ef9a0eef2088ca27ae6f5f862c28389
zimina.t.04@mail.ru <zimina.t.04@mail.ru>
2022-06-02 17:57:29 +0300

    mew

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,16 +7,25 @@ namespace MvcMovie.Models
 {
     public class Movie
     {
-            public int Id { get; set; }
-            public string Title { get; set; }
+        public int Id { get; set; }
 
-            [Display(Name = "Release Date")]
-            [DataType(DataType.Date)]
-            public DateTime ReleaseDate { get; set; }
-            public string Genre { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Title { get; set; }
 
-            [Column(TypeName = "decimal(18, 2)")]
-            public decimal Price { get; set; }
-            public string Rating { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
     }
 }
--- SeedData/SeedData.cs
+++ SeedData/SeedData.cs
@@ -51,14 +51,6 @@ namespace MvcMovie.Models
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
                         Price = 3.99M
-                    },
-                    new Movie
-                    {
-                        Title = "When Harry Met Sally",
-                        ReleaseDate = DateTime.Parse("1989-1-11"),
-                        Genre = "Romantic Comedy",
-                        Rating = "R",
-                        Price = 7.99M
                     }
                 );
                 context.SaveChanges();
